976467c5c9e5bd5602026f28bc83b73aec651eff
pnkn <waynehammond96@gmail.com>
2018-03-08 22:05:47 -0500

    attempt 1 at fitting eds site into working example of django, didn't go as well as it should've

---
+++ djangoautic/assets/eds.js
@@ -0,0 +1,54 @@
+"use strict";
+
+// START slideshow code
+let i = 0;
+let images = [];
+let time = 3000;
+
+images[0] = "static/img/bag_eds.jpg";
+images[1] = "static/img/jimcactus.png";
+images[2] = "static/img/backg.jpg";
+images[3] = "static/img/squid.jpg";
+
+
+
+
+
+// change image
+function changeImg() {
+	document.slide.src = images[i];
+
+	if (i < images.length -1 ) {
+		i++;
+	} else {
+		i = 0;
+	}
+	setTimeout("changeImg()", time);
+}
+
+
+// next button
+function nextImage() {
+	if (i < images.length - 1) {
+		i++;
+		document.slide.src = images[i];
+	} else {
+		i = 0;
+		document.slide.src = images[i];
+	}
+}
+
+// previous button
+function prevImage() {
+	if (i != 0) {
+		i--;
+		document.slide.src = images[i];
+	} else {
+		i = images.length - 1;
+		document.slide.src = images[i];
+	}
+}
+
+
+window.onload = changeImg;
+// END of slideshow
